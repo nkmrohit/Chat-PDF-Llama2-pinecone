cf49403b6677294247628118733cc98f3e078720
nkmrohit <neerajmaurya.21mnnit@gmail.com>
2023-09-09 14:26:57 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,58 @@
+#####Below code only run on GPU Server, It is also working on google colab, vector dimension is 768 , We can login pine cone vector data base by gmail login
+
+import os
+import sys
+import pinecone
+from langchain.llms import Replicate
+from langchain.vectorstores import Pinecone
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.document_loaders import PyPDFLoader
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.chains import ConversationalRetrievalChain
+
+# Replicate API token
+os.environ['REPLICATE_API_TOKEN'] = "r8_WX5mSgP6EZ0QIKeMQnwmlTyubSm1cj81w3G0y"
+
+# Initialize Pinecone
+pinecone.init(api_key='f060f7c5-5e62-45dd-b4db-56db86b10b3f', environment='us-west4-gcp-free')
+
+# Load and preprocess the PDF document
+loader = PyPDFLoader('./Updaed_Resume.pdf')
+documents = loader.load()
+
+# Split the documents into smaller chunks for processing
+text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+texts = text_splitter.split_documents(documents)
+
+# Use HuggingFace embeddings for transforming text into numerical vectors
+embeddings = HuggingFaceEmbeddings()
+# Set up the Pinecone vector database
+index_name = "llama2-pdf"
+index = pinecone.Index(index_name)
+vectordb = Pinecone.from_documents(texts, embeddings, index_name=index_name)
+
+# Initialize Replicate Llama2 Model
+llm = Replicate(
+    model="a16z-infra/llama13b-v2-chat:df7690f1994d94e96ad9d568eac121aecf50684a0b0963b25a41cc40061269e5",
+    input={"temperature": 0.75, "max_length": 3000}
+)
+
+# Set up the Conversational Retrieval Chain
+qa_chain = ConversationalRetrievalChain.from_llm(
+    llm,
+    vectordb.as_retriever(search_kwargs={'k': 2}),
+    return_source_documents=True
+)
+
+chat_history = []
+while True:
+    query = input('Prompt: ')
+    if query.lower() in ["exit", "quit", "q"]:
+        print('Exiting')
+        sys.exit()
+    result = qa_chain({'question': query, 'chat_history': chat_history})
+    print('Answer: ' + result['answer'] + '\n')
+    chat_history.append((query, result['answer']))
+    
+    
+    
